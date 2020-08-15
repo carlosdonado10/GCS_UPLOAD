0049d2d803d3267aaee69c9972b1261d574bb6c0
carlosdonado <cf.donado@hotmail.com>
2020-08-14 23:16:38 -0500

    Added rotations for fold2_part1

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import cv2
 
 rotations = {
+    13: cv2.cv2.ROTATE_90_COUNTERCLOCKWISE,
+    14: None,
+    15: None,
+    16: None,
+    17: None,
+    18: None,
     55: None,  # Not necessary
     56: cv2.cv2.ROTATE_90_CLOCKWISE,
     57: cv2.cv2.ROTATE_90_CLOCKWISE,
